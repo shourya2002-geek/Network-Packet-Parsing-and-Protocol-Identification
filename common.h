7c119142211038823f736e4150bef988106fa064
Shourya Gupta <75240318+shourya2002-geek@users.noreply.github.com>
2022-11-14 15:40:12 +0530

    Create common.h

---
+++ common.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <net/ethernet.h>
+#include <netdb.h>
+#include <netinet/if_ether.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/ip6.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+
+#define fd_t unsigned int
+#define sa_size sizeof(struct sockaddr)
